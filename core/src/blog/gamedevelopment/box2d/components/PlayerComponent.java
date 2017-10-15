d16566ffd2f0b38c9c32858474d7ce7a4d50a0f8
Fernando Hernandez <fefehern@hotmail.com>
2017-10-15 17:54:50 -0400

    Ashley Entity Update

---
+++ core/src/blog/gamedevelopment/box2d/components/PlayerComponent.java
@@ -0,0 +1,6 @@
+package blog.gamedevelopment.box2d.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class PlayerComponent implements Component {
+}
---
+++ core/src/blog/gamedevelopment/box2d/components/TypeComponent.java
@@ -0,0 +1,15 @@
+package blog.gamedevelopment.box2d.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Stores the type of entity this is
+ */
+public class TypeComponent implements Component {
+    public static final int PLAYER = 0;
+    public static final int ENEMY = 1;
+    public static final int SCENERY = 2;
+    public static final int OTHER = 4;
+
+    public int type = OTHER;
+}
